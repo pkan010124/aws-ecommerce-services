80dade85027561f435491f1f681a08d2a4b6de24
Prasad Kanigicherla <pkanigic@gmail.com>
2024-01-31 22:45:26 -0500

    added some product lambda files

--- lib/aws-ecommerce-services-stack.ts
+++ lib/aws-ecommerce-services-stack.ts
@@ -1,16 +1,72 @@
 import * as cdk from 'aws-cdk-lib';
+import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
+import { Table, AttributeType, BillingMode } from 'aws-cdk-lib/aws-dynamodb';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunctionProps, NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
+import { join } from 'path';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsEcommerceServicesStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // create product table
+    const productTable = new Table(this, 'product', {
+      partitionKey: {
+        name: 'id',
+        type: AttributeType.STRING
+      },
+      tableName: 'product',
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      billingMode: BillingMode.PAY_PER_REQUEST
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsEcommerceServicesQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const nodeJsFunctionProps: NodejsFunctionProps = {
+      bundling: {
+        externalModules: [
+          'aws-sdk'
+        ]
+      },
+      environment: {
+        PRIMARY_KEY: 'id',
+        DYNAMODB_TABLE_NAME: productTable.tableName
+      },
+      runtime: Runtime.NODEJS_20_X
+    }
+
+    // Product microservices lambda function
+    const productFunction = new NodejsFunction(this, 'productLambdaFunction', {
+      entry: join(__dirname, `/../src/product/index.js`),
+      ...nodeJsFunctionProps,
+    })
+
+    productTable.grantReadWriteData(productFunction);
+
+    // Product microservices api gateway
+    // root name = product
+
+    // GET /product
+    // POST /product
+
+    // Single product with id parameter
+    // GET /product/{id}
+    // PUT /product/{id}
+    // DELETE /product/{id}
+
+    const apigw = new LambdaRestApi(this, 'productApi', {
+      restApiName: 'Product Service',
+      handler: productFunction,
+      proxy: false
+    });
+
+    const product = apigw.root.addResource('product');
+    product.addMethod('GET'); // GET /product
+    product.addMethod('POST'); // POST /product
+
+    const singleProduct = product.addResource('{id}'); // product/{id}
+    singleProduct.addMethod('GET');  // GET product/{id}
+    singleProduct.addMethod('PUT');  // PUT product/{id}
+    singleProduct.addMethod('DELETE');  // DELETE product/{id}
   }
 }
